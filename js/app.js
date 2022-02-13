582f9e65992c18854310004a9182ae54b9490c82
srabon0 <srabonemam4@gmail.com>
2022-02-13 16:25:00 +0600

    pin matched tested

--- js/app.js
+++ js/app.js
@@ -23,8 +23,23 @@ document.getElementById('keypad').addEventListener('click',function(event){
         console.log(currentInputValue,clickedNum);
         const newNum = currentInputValue+clickedNum;
         typedInput.value = newNum;
-        console.log(newNum)
+        console.log(newNum);
     
     }
    
 })
+
+// check pins both field
+function matchPIN(){
+    const currentGenPin = document.getElementById('otp-holder').value;
+    const typedPin = document.getElementById('typed-nums').value;
+    const failed = document.getElementById('failed');
+    const success = document.getElementById('success');
+    if (currentGenPin == typedPin){
+        failed.style.display = 'none';
+        success.style.display = 'block';
+    }else{
+        failed.style.display = 'block';
+        success.style.display = 'none';
+    }
+}
